7d262f73b6a8d4a9d8ba611521421248bf110997
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-04-19 10:42:35 -0500

    Add useContext and useReducer for logged user

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
+
+import Context from "../../context"
 
 const ME_QUERY = `
   {
@@ -16,6 +18,7 @@ const ME_QUERY = `
 `
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context)
   const onSuccess = async googleUser => {
     //console.log({ googleUser });
     const idToken = googleUser.getAuthResponse().id_token;
@@ -23,7 +26,11 @@ const Login = ({ classes }) => {
       headers: { authorization: idToken }
     });
     const data = await client.request(ME_QUERY)
-    console.log(data);
+    //console.log(data);
+    dispatch({
+      type: "LOGIN_USER",
+      payload: data.me
+    })
   };
 
   const onFailure = error => {
